b50869967ae0c5decab2689c204b8cfab1251a94
studycloud111 <74172237+studycloud111@users.noreply.github.com>
2022-11-18 21:18:06 +0800

    Create check_ddns.sh

---
+++ check_ddns.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+rm -rf  /root/ipd.sh
+wget -P /root/ipd.sh https://raw.githubusercontent.com/studycloud111/saveblocklist/main/ddns.sh
+#写入定时任务文件
+ipddns(){
+cat > /root/ipddns << EOF
+10.10.10.10
+EOF
+}
+#写入定时任务文件
+cron_file(){
+cat > /usr/lib/systemd/system/ddns.service << EOF
+[Unit]
+Description=DDNS_check
+
+[Service]
+ExecStart=/bin/bash /root/ipd.sh
+
+EOF
+
+
+cat > /usr/lib/systemd/system/ddns.timer << EOF
+
+[Unit]
+Description=Runs mytimer 5 min
+
+[Timer]
+OnBootSec=5min
+OnUnitActiveSec=5min
+Unit=ddns.service
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+systemctl start ddns.service
+systemctl start ddns.timer
+systemctl reload
+} 
+ipddns
+cron_file
