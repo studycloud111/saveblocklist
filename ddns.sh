f2c0971588ae1e6062e14effa6b055125c1944bc
studycloud111 <74172237+studycloud111@users.noreply.github.com>
2022-11-18 19:55:48 +0800

    Create ddns.sh

---
+++ ddns.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+myip=$(curl ip.qaros.com | awk 'NR==1')
+twip=$(cat /root/ipddns | awk 'NR==1')
+if [ ${myip} = ${twip} ]; then
+    echo "ip没有变化,不需要重启"
+else
+    systemctl restart nginx
+cat > /root/ipddns << EOF
+    ${myip}
+EOF
+    echo "nginx重启成功"
+fi
