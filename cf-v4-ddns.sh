6ee85fb783e014adb180e937f1b216c59c9c2f63
studycloud111 <74172237+studycloud111@users.noreply.github.com>
2023-05-19 21:49:15 +0800

    Update cf-v4-ddns.sh

--- cf-v4-ddns.sh
+++ cf-v4-ddns.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 set -o nounset
-set -o pipefail
 
 # Automatically update your CloudFlare DNS record to the IP, Dynamic DNS
 # Can retrieve cloudflare Domain id and list zone's, because, lazy
